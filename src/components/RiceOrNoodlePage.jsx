845b7548d7871350a784164bbe080ccb0943edf9
Panpaphop Pholprateepsuriya <ryu@MacBook-Air-khxng-Panpaphop.local>
2023-11-13 12:09:51 +0700

    fix:fix minor margin

--- src/components/RiceOrNoodlePage.jsx
+++ src/components/RiceOrNoodlePage.jsx
@@ -13,7 +13,7 @@ const RiceOrNoodlePage = () => {
     return (
       <div className="bg-blue-200 rounded-lg w-[450px] h-[800px] flex flex-col justify-center items-center">
         <h1 className="text-[25px] font-bold">ข้าวหรือเส้น ?</h1>
-        <button className="image-button text-[25px] font-bold border border-solid border-black m-3 ml-8 mr-8 bg-gray-200 rounded-lg flex flex-row justify-around items-center focus:bg-gray-300 "onClick={() => handleButtonClick("rice")}>
+        <button className="image-button text-[25px] font-bold border border-solid border-black mt-14 mb-3 ml-8 mr-8 bg-gray-200 rounded-lg flex flex-row justify-around items-center focus:bg-gray-300 "onClick={() => handleButtonClick("rice")}>
           <img src={riceIcon} alt="Spicy" className="w-[30%] m-5" /> ข้าว
         </button>
         <button className="image-button text-[25px] font-bold border border-solid border-black m-3 ml-8 mr-8 bg-gray-200 rounded-lg flex flex-row justify-around items-center focus:bg-gray-300"onClick={() => handleButtonClick("noodle")}>
--- src/components/ShufflePage.jsx
+++ src/components/ShufflePage.jsx
@@ -62,7 +62,7 @@ const ShufflePage = () => {
     return (
       <div className="bg-blue-200 rounded-lg w-[450px] h-[800px] flex flex-col justify-center items-center">
         <h1 className="text-[25px] font-bold">กินอะไรดี?</h1>
-        <div className="text-[25px] h-[40%] font-bold border border-solid border-black m-3 ml-8 mr-8 bg-white rounded-lg flex flex-col justify-around items-center">
+        <div className="text-[25px] h-[40%] font-bold border border-solid border-black mt-14 mb-3 ml-8 mr-8 bg-white rounded-lg flex flex-col justify-around items-center">
           <img src={foodIcon} alt="Spicy" className="w-[30%] m-5" /> {shuffledFood ? shuffledFood.name : "กด Shuffle เพื่อสุ่ม"}
         </div>
         <button className="image-button text-[25px] font-bold border border-solid border-black m-3 ml-8 mr-8 bg-gray-200 rounded-lg flex flex-row justify-around items-center focus:bg-gray-300"onClick={() => shuffleFood()}>
